886c3914d87a5d6b0cfb4e6c0b9be739fc0b6c28
Jason Gegere <jason+github@htmlgraphic.com>
2014-09-30 09:58:27 -0500

    Update twiml.php
    Correction for https://github.com/twilio/OpenVBX/issues/284#issuecomment-57211592

--- plugins/sms/applets/sms/twiml.php
+++ plugins/sms/applets/sms/twiml.php
@@ -3,7 +3,7 @@ $sms = AppletInstance::getValue('sms');
 $next = AppletInstance::getDropZoneUrl('next');
 
 $response = new TwimlResponse;
-$response->message($sms);
+if(AppletInstance::getFlowType() == 'voice') $response->sms($sms); else $response->message($sms);
 if(!empty($next))
 {
 	$response->redirect($next);
